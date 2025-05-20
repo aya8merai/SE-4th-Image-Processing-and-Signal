3db8c22cab403cab934edc9c0c62562a91a0d1ba
Aya H. Merai <aya5312.merai@gmail.com>
2025-05-20 12:11:07 +0300

    Update and rename IPS1_Lab3_DFT_1D_Img.m to IPS1_Lab4_DFT_1D_Img.m
     Apply Discrete Fourier Transform (1 Dimension) to the image

--- IPS1_Lab4_DFT_1D_Img.m
+++ IPS1_Lab4_DFT_1D_Img.m
@@ -22,5 +22,16 @@ fnew(x) = 0;
 for u = 1: N 
 fnew(x) = fnew(x) + F(u) * exp(2j * pi * (u-1) * (x-1)/ N);         
 end 
+
+f_original = reshape(f, r, c); 
+f2d_spect = reshape(spect, r, c); 
+Fshift = fftshift(f2d_spect); 
+f_inv = reshape(fnew, r, c); 
+figure 
+subplot(2,2,1),imshow(uint8(f_original)),title('Original'); 
+subplot(2,2,2),imshow(f2d_spect),title('spectrum'); 
+subplot(2,2,3),imshow(Fshift),title('spectrum with shift'); 
+subplot(2,2,4),imshow(uint8(f_inv)),title('after inverse'); 
+
 end 
 fnew = round(fnew); 
